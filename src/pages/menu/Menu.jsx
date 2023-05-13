f1f4fa920bed2dbb21060d2d164600e6cbb0ca9c
itzshakeeb <itzshakeeb@gmail.com>
2023-05-14 00:20:18 +0600

    done footer

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -23,7 +23,7 @@ const MenuPage = () => {
                                     <div className="h-[70vh] flex items-center justify-center">
                                         <ScaleLoader color="#000000" />
                                     </div>
-                                    : <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                                    : <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                                         {products.map((product) => (
                                             <div className="" key={product?.id}>
                                                 <Card product={product} />
